51ca46d74856ffdc737182ce1db23cf78866fedf
Catalin Moldovan <moldovan.catalin@gmail.com>
2023-05-21 16:35:13 +0300

    different packing

--- Bake3DCursorOperator.py
+++ Bake3DCursorOperator.py
@@ -1,31 +1,75 @@
 import bpy
 from bmesh import from_edit_mesh, update_edit_mesh
+import struct
 
 
-# keeps only 3 digits resolution
-def packTwoFloats(a, b):
-    # Pack two floats in the range of 0-1 into one float
-    aScaled = a * 1000.0
-    bScaled = b * 1000.0
-    abPacked = (int(aScaled) << 16) | (
-        int(bScaled) & 0xFFFF
-    )  # Combine a and b into one unsigned 32-bit integer
-    finalFloat = float(abPacked)
-    return finalFloat
+def packTwoFloats(float1, float2):
+    # Convert two single-precision floats to half-precision floats
+    half1 = struct.unpack("H", struct.pack("e", float1))[0]
+    half2 = struct.unpack("H", struct.pack("e", float2))[0]
+
+    # Pack the half-precision floats into a single-precision float
+    combined_half = ((half2 & 0xFFFF) << 16) | half1
+
+    # Convert the packed value to a single-precision float
+    packed_float = struct.unpack("f", struct.pack("I", combined_half))[0]
+
+    return packed_float
 
 
-# keeps only 3 digits resolution
-def unpackTwoFloats(abPacked):
-    abPackedInt = int(abPacked)
-    aScaled = (
-        abPackedInt >> 16
-    ) & 0xFFFF  # Extract the upper 16 bits as an unsigned 16-bit integer
-    bScaled = (
-        abPackedInt & 0xFFFF
-    )  # Extract the lower 16 bits as an unsigned 16-bit integer
-    a = float(aScaled) / 1000.0  # Scale a back to a float
-    b = float(bScaled) / 1000.0  # Scale b back to a float
-    return (a, b)
+def unpackTwoFloats(packed_float):
+    # Convert the single-precision float to a packed binary string
+    packed_binary = struct.pack("f", packed_float)
+
+    # Unpack the binary string as an unsigned integer
+    combined_half = struct.unpack("I", packed_binary)[0]
+
+    # Extract the individual half-precision floats
+    half1 = combined_half & 0xFFFF
+    half2 = (combined_half >> 16) & 0xFFFF
+
+    # Convert the half-precision floats to single-precision floats
+    float1 = struct.unpack("e", struct.pack("H", half1))[0]
+    float2 = struct.unpack("e", struct.pack("H", half2))[0]
+
+    return float1, float2
+
+
+def pack_floats(float1, float2):
+    # Scale the floats to the range of -100 to +100
+    scaled1 = (float1 + 100) / 200  # Scale to 0-1 range
+    scaled2 = (float2 + 100) / 200  # Scale to 0-1 range
+
+    # Convert the scaled floats to integers within the range of 0 to 65535 (2^16 - 1)
+    int1 = int(scaled1 * 65535)
+    int2 = int(scaled2 * 65535)
+
+    # Pack the two integers into a single 32-bit integer
+    packed_int = (int2 << 16) | int1
+
+    # Convert the packed integer to a float
+    packed_float = struct.unpack("f", struct.pack("I", packed_int))[0]
+
+    return packed_float
+
+
+def unpack_float(packed_float):
+    # Convert the packed float to a packed integer
+    packed_int = struct.unpack("I", struct.pack("f", packed_float))[0]
+
+    # Extract the two packed integers from the packed integer
+    int1 = packed_int & 0xFFFF
+    int2 = (packed_int >> 16) & 0xFFFF
+
+    # Convert the packed integers back to scaled floats
+    scaled1 = int1 / 65535
+    scaled2 = int2 / 65535
+
+    # Rescale the floats to the original range of -100 to +100
+    float1 = scaled1 * 200 - 100
+    float2 = scaled2 * 200 - 100
+
+    return float1, float2
 
 
 class Bake3DCursorOperator(bpy.types.Operator):
@@ -55,14 +99,23 @@ class Bake3DCursorOperator(bpy.types.Operator):
             if activeUvLayer is not None:
                 print("active uv layer:", activeUvLayer.name)
                 uv = (
-                    packTwoFloats(cursor.x, cursor.y),
-                    packTwoFloats(cursor.z, 1.0),
+                    pack_floats(cursor.x, cursor.y),
+                    pack_floats(cursor.z, 1.0),
                 )
                 for i in vsel:
                     v = bm.verts[i]
                     for l in v.link_loops:
                         # print(l[activeUvLayer])
                         l[activeUvLayer].uv = uv
+                        # print("uv: ", l[activeUvLayer].uv)
+                        print(
+                            "vertex: ",
+                            v,
+                            "uv: ",
+                            uv,
+                            "unpacked: ",
+                            (unpack_float(uv[0]), unpack_float(uv[1])),
+                        )
                 update_edit_mesh(obj.data)
 
         bm.free()
